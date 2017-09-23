c8931da5710af784607f1ef48fbf48fc559206f0
Mas Luky <masluky268@gmail.com>
2017-09-23 16:36:05 +0700

    Upload index.html dan main.js

---
+++ main.js
@@ -0,0 +1,89 @@
+// // // document.write("Hello word!");
+// // //
+// // // // var angka=5;//type dat angka
+// // // // var text='anu itu apa';//type dat string
+// // // // var bool=true;//type boolean
+// // // //
+// // // // angka=angka+angka;
+// // // // angka+=angka;
+// // // //
+// // // // document.write('<br>angka : '+angka)//<br> kanggo enter front end
+// // // document.write('<br>text : '+text)//\n kanggo enter nang console
+// // // document.write('<br>boolean : '+bool)
+// // //
+// // //
+// // // console.log("Hello log");
+// //
+// // document.getElementById('tes').innerHTML="aku paragraf"
+// //
+// // var text1=document.getElementById('text1').innerText
+// // var text2=document.getElementById('text2').innerText
+// //
+// // //ganbung ke 1 variable
+// // var total=parseInt(text1)+parseInt(text2)
+// // //parseInt(parameter)->membuat string menjadi int note: kecuali ono hurufe ora iso diparse int
+// // document.getElementById('gabung').innerHTML= text1+'+'+text2+'='+total
+//
+// // var angka1=69
+// // var angka2=96
+// //
+// // console.log('apakah '+angka1+'<'+angka2+':\n');
+// // if(angka1<angka2){
+// //   if(angka1==69){
+// //     console.log('bazeng');
+// //   }console.log('benar');
+// // }else {
+// //   if(angka2==69){
+// //     console.log('hmm');
+// //   }else {
+// //     console.log('jangkrik');
+// //   }console.log('salah');
+// // }
+//
+// //------------ looping -------------
+//
+// for (i = 0; i < 5; i++) {
+//     console.log('the number'+i);
+// }
+// var i=0;
+// while (i<5) {
+//     console.log('Number '+i);
+//     i++
+// }
+//
+// //nested loop
+// var i;
+// for(i=0;;i++){
+//   if(i==9){
+//     break
+//   }else {
+    //  console.log(i);
+//   }
+//   if(i==5){
+//     break
+//   }
+// }
+// function tambah(a,b){
+//   return a+b;
+// }
+//
+// function cetak(){
+//   console.log('coba aja');
+// }
+//
+// //tambah
+//
+// var x=1
+// var y=1
+//
+// var isAku=function(){
+//   if(tambah(x,y)=='oraAku'){
+//     return true;
+//   }else{
+//     return false;
+//   }
+// }
+// console.log(isAku()?'aku benar':'aku salah');//tanda (?) menanyakan fungsi boolean. true / false
+
+var input=prompt()
+  console.log(input);
